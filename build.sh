c7607f1673dc99740334a927d7a6e89d0e414342
Jason Chin <cschin@infoecho.net>
2026-04-10 09:48:35 -0700

    build: remove obsolete LIBCLANG_PATH comment and redundant pgr-db build step

--- build.sh
+++ build.sh
@@ -1,14 +1,8 @@
 rustup default stable
 
-
-## if necessary, you can instal libclang / clang using Anaconda 
-## and set LIBCLANG_PATH to point to the libclang for cbindgen dependence clang-sys
-#export LIBCLANG_PATH=$HOME/miniconda3/lib
-
 ## if necessary, install maturin with `cargo install --locked maturin`
 # cargo install --locked maturin
 
-cargo build -p pgr-db --release
 cargo build -p pgr-bin --release
 cargo install --path pgr-bin
 
