e7ace4fafa2200c704c64c3b8c89463117d23d86
Jason Chin <cschin@infoecho.net>
2026-04-10 17:28:36 -0700

    build: add agc-rs build and install steps to build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@ rustup default stable
 ## if necessary, install maturin with `cargo install --locked maturin`
 # cargo install --locked maturin
 
+cargo build -p agc-rs --release
+cargo install --path agc-rs
+
 cargo build -p pgr-bin --release
 cargo install --path pgr-bin
 
